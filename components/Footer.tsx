52389e5b3bba34a7764091389f7d6d3e33289f2e
ramadhaneki <ramadhaneki91@gmail.com>
2023-02-21 02:24:11 +0700

    add maein menu

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { footerList1, footerList2, footerList3 } from '../utils/constants'
 
 function Footer() {
+
+  const List = ({items, mt}: { items: string[], mt: boolean }) => (
+    <div className={`flex flex-warp ${mt && 'mt-5'} gap-2  mt-5`}>
+        {items.map((item) => (
+          <p key={item} className='text-gray-400 text-sm hover:underline cursor-pointer'>
+            {item}
+          </p>
+        ))}
+    </div>
+  )
+
   return (
-    <div>
-      Footer
+    <div className='mt-6 hidden xl:block'>
+      <List items={footerList1} mt={true}/>
+      <List items={footerList2} mt/>
+      <List items={footerList3} mt/>
+      <p className='text-gray-400 text-sm mt-5'>2023 Eki Ramadhan</p>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import axios from 'axios'
+
+// interface IProps {
+//   videos: 
+// }
+
+export default function Home({videos}: {videos: string[]}) {
+  console.log(videos)
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   )
+}
+
+export const getServerSideProps = async ( ) => {
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
